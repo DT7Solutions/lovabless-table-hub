1b79cc31d3657085da3a1cfb71763db2ee128d36
Chaitanya Kothakota <120007815+chaitanyakothakota@users.noreply.github.com>
2025-10-23 19:32:00 +0530

    change favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: mode === "production" ? "/lovabless-table-hub/" : "/",
+ 
+  base: mode === "production" ? "/lovabless-table-hub/" : "/",
   server: {
     host: "::",
     port: 8080,
